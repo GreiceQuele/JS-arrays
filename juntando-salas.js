e5285ff5cea0f889a4906b15abc179c51e6424c2
Quele <greice.caetano@gmail.com>
2023-05-16 14:00:29 -0300

    Atividades do curso JS arrays

---
+++ juntando-salas.js
@@ -0,0 +1,6 @@
+const salajs = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const salasUnificadas = salajs.concat(salaPython);
+
+console.log(salasUnificadas);
---
+++ listas-duas-dimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+const listaDeAlunosEMedia = [alunos, medias];
+
+console.log(`A aluna da posição 1 é a: ${listaDeAlunosEMedia[0][1]}
+    A nota desta aluna é: ${listaDeAlunosEMedia[1][1]}
+    
+`);
