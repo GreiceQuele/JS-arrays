dc2220b5e97d690c2796510c50e51dd9cc9b75b2
Quele <greice.caetano@gmail.com>
2023-05-16 14:39:56 -0300

    exemplos de como utilizar o includes e o indexOf

---
+++ procurando-aluno.js
@@ -0,0 +1,18 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+const listaDeAlunosEMedia = [alunos, medias];
+
+function exibeNomeENota (aluno) {
+    if(listaDeAlunosEMedia[0].includes(aluno)) {
+        const indice = listaDeAlunosEMedia[0].indexOf(aluno);
+        const mediaDoAluno = listaDeAlunosEMedia[1][indice];
+
+        console.log(`${aluno}, tem a média final ${mediaDoAluno}.`);
+
+    }else {
+        console.log(`Aluno não esta cadastrado!`);
+        
+    }
+}
+
+exibeNomeENota('João');
