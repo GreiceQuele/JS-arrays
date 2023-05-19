c6ba1519d80f44a9b9acd234696fe3608bbcf053
Quele <greice.caetano@gmail.com>
2023-05-19 12:03:21 -0300

    exercicíos sobre callback e maps

---
+++ maps-nomes.js
@@ -0,0 +1,8 @@
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+//const nomesEmMaiusculos = nomes.map((nome) => {
+    //return nome.toUpperCase();} )
+
+const nomesEmMaiusculos = nomes.map((nome) => nome.toUpperCase()) ;  //mais usado no mercado 
+
+console.log(nomesEmMaiusculos);
